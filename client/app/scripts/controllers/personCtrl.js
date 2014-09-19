d4c2b00eca382f8216596e934cb5f9bceba1de66
Lisa <halvorsen.lisa@gmail.com>
2014-09-19 08:24:10 +0200

    lagt til autocomplete, put kjenner (må endres til å sende id nummeret), post og get person fungerer, getAllPersons fungerer

--- client/app/scripts/controllers/personCtrl.js
+++ client/app/scripts/controllers/personCtrl.js
@@ -1,86 +1,126 @@
 (function(app){
-	'use strict';
+    'use strict';
 
-  app.controller('PersonCtrl', ['$scope', '$location', '$http', 'PersonService', 'HentPerson', 'Person',
-  	function ($scope, $location, $http, PersonService, HentPerson, Person) {
-  	$scope.submitperson = function() {
-  		alert("Something submitted");
-  	}
-  	$scope.method = 'POST';
-  	$scope.url = CONFIG.API_BASE_URL + "service/uib/person";
-  	$scope.venner = PersonService.getAllPersons();
-  	$scope.selected;
-  	$scope.graph = {};
-  	$scope.wholeGraph = {};
-  	
-  	$http({method: 'GET', url: $scope.url}).success(function(response){
-  		console.log(response);
-  		$scope.wholeGraph = response;
-  	});
-	var persons = Person.getAllPersons({}, function() {
-		console.log(persons)	
-	}); 
-	$scope.test = {"@c": "lalal"} 	
+    app.controller('PersonCtrl', ['$scope', '$location', '$http', 'PersonService', 'HentPerson', 'Person', 'Kjenner',
+        function ($scope, $location, $http, PersonService, HentPerson, Person, Kjenner) {
+            $scope.submitperson = function() {
+                alert("Something submitted");
+            }
+            $scope.method = 'POST';
+            $scope.url = CONFIG.API_BASE_URL + "service/uib/person";
+            $scope.venner = PersonService.getAllPersons();
+            $scope.selected;
+            $scope.graph = {};
+            $scope.wholeGraph = {};
+
+            $http({method: 'GET', url: $scope.url}).success(function(response){
+                console.log(response["@graph"]);
+                console.log(response);
+                $scope.wholeGraph = response["@graph"];
+            });
+            var persons = Person.getAllPersons({}, function() {
+                console.log(persons)
+            });
+            $scope.test = {"@c": "lalal"}
 //  	Person.get({}, function())
-	// Person.query().$promis.then(function (persons) { //getAllPersons({}, function(person){
-	// 	console.log(persons);
-	// 	//person.$promis.then(function(persons) {
-	// 	//	console.log(persons);
-	// 	})
+            // Person.query().$promis.then(function (persons) { //getAllPersons({}, function(person){
+            // 	console.log(persons);
+            // 	//person.$promis.then(function(persons) {
+            // 	//	console.log(persons);
+            // 	})
 
 
-	// }
-  //, function(err) {
+            // }
+            //, function(err) {
 
-	//});
-  	$scope.submitperson = function() {
-  		console.log("submitting person");
-  		var person = retrievePerson();
-  		$http({
-    		method: 'POST',
-    		url: $scope.url,
-    		data: $.param({fornavn: person.fornavn, 
-    			etternavn: person.etternavn,
-    			alder: person.alder,
-    			studieretning: person.studieretning}),
-    		headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-		}).success(function(response){
-			console.log("Suksess");
-			console.log(response);
-			$scope.graph = response;
-			//console.log(response.@graph);
-			//console.log(response.@context);
-		});
-  		
-  		// var newPerson = new Person(person);
-  		// console.log("newPerson");
-  		// console.log(newPerson);
-  		// var anNewThinf = newPerson.$save();
-  		// console.log("aNewThing");
-  		// console.log(anNewThinf);
+            //});
 
-  		// new Person(person).$createPerson({
+            $scope.addKjenner = function (inputString) {
+                console.log("addKjenner");
+                console.log(inputString);
 
-  		// }, function(person) {
-  		// 	//hva som skjer når funksjon returnerer
-  		// 	console.log("Suksess");
-  		// 	console.log(person);
-  		// 	clearPersonInput();
-  		// }, function(err) {
-  		// 	//error
-  		// 	$scope.error = "Noe gikk galt :(";
-  		// });
-  	};
-  	function retrievePerson() {
-  		var person = {};
-  		person.fornavn = $scope.person.fornavn;
-  		person.etternavn = $scope.person.etternavn;
-  		person.alder = $scope.person.alder;
-  		person.studieretning = $scope.person.studieretning;
-  		return person;
-  	}
-  	function clearPersonInput() {
-  		$scope.person = {};
-  	}
-  }]); 
+                new Kjenner(inputString).$addKjenner({
+                    id: $scope.currentPerson["@id"],
+                    kjenner_id: inputString["@id"]
+                }, function () {
+                    $scope.feedbackTitle = "Your message has been edited";
+                    HentPerson.hentPerson({
+                        id: $scope.currentPerson["@id"]
+                    }, function(person){
+                        $scope.currentPerson = person;
+                    }, function(err) {
+                        console.log("Noe galt med hent person etter lagt til kjenner");
+                    });
+                }, function (err) {
+                    $scope.error = "Something went wrong ;(";
+                    console.log("Noe galt med legge til kjenner");
+                });
+
+            };
+            $scope.selectedInputFormatter = function () {
+                return "";
+            };
+            $scope.putKjenner = function(node) {
+                //todo legge inn in kjenner inputbox
+            };
+            $scope.lagreKjenner = function(node){
+                //$http({method: 'POST', url: $scope.url + :id/kjenner/:kjenner_id})
+
+
+
+            };
+            $scope.onMemberSelect = function (item) {
+                $scope.addKjenner(item);
+            };
+
+            $scope.submitperson = function() {
+                console.log("submitting person");
+                var person = retrievePerson();
+                $http({
+                    method: 'POST',
+                    url: $scope.url,
+                    data: $.param({fornavn: person.fornavn,
+                        etternavn: person.etternavn,
+                        alder: person.alder,
+                        studieretning: person.studieretning}),
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                }).success(function(response){
+                    console.log("Suksess");
+                    console.log(response);
+                    $scope.currentPerson = response["@graph"][0];
+                    //console.log(response.@graph);
+                    //console.log(response.@context);
+                });
+
+                // var newPerson = new Person(person);
+                // console.log("newPerson");
+                // console.log(newPerson);
+                // var anNewThinf = newPerson.$save();
+                // console.log("aNewThing");
+                // console.log(anNewThinf);
+
+                // new Person(person).$createPerson({
+
+                // }, function(person) {
+                // 	//hva som skjer når funksjon returnerer
+                // 	console.log("Suksess");
+                // 	console.log(person);
+                // 	clearPersonInput();
+                // }, function(err) {
+                // 	//error
+                // 	$scope.error = "Noe gikk galt :(";
+                // });
+            };
+            function retrievePerson() {
+                var person = {};
+                person.fornavn = $scope.person.fornavn;
+                person.etternavn = $scope.person.etternavn;
+                person.alder = $scope.person.alder;
+                person.studieretning = $scope.person.studieretning;
+                return person;
+            }
+            function clearPersonInput() {
+                $scope.person = {};
+            }
+        }]);
 })(angular.module('app'));
--- client/app/scripts/services/personService.js
+++ client/app/scripts/services/personService.js
@@ -1,36 +1,65 @@
 (function(app){
+
 'use strict';
-	app.factory('PersonService', ['$http', function($http){
-	var baseUrl = "uib/person/";
-    var PersonService = {};
-    PersonService.getAllPersons = function(){
-        return $http.get(baseUrl);
-    };
-    PersonService.getPerson = function(id) {
-        return $http.get(baseUrl + id);
-    };
-    PersonService.createPerson = function() {
-        return $http.post();
-    }
-    PersonService.putKjenner = function(id, kjennerId) {
-        return $http.put(baseUrl + id +"/kjenner/"+ kjennerId);
-    }
 
-    app.factory('Movie', ['$resource', function($resource){
-        return $resource(CONFIG.API_BASE_URL + "movies", {}, {
-           getMovies: {
-               method: 'GET'
-           },
-            getMovie: {
-                method: 'GET'
-            },
-            addMovie: {
-                method: 'POST'
-            }
-        });
-    }]);
+app.factory('PersonService', [ function() {
+	var PersonService = {
+		getAllPersons: function() {
+			return [
+				"Lisa", "Lene", "Max", "Mari"
+				//{firstName: "Lisa", lastName: "Halvorsen"},
+				//{firstName: "Max", lastName: "Muller"}
+				];
+		},
+		getPerson: function(id) {
+			return {firstName: "Lisa", lastName: "Halvorsen"}
+		},
+		createPerson: function(person) {
+			console.log("Person: " + person);
+			
+		}	
+	};
 
-        
-    return PersonService;
+	return PersonService;		
+
 	}]);
+
+app.factory('Person',['$resource', function($resource){
+	return $resource(CONFIG.API_BASE_URL + "service/uib/person", {}, {
+		getAllPersons: {
+			method: 'GET'
+			
+		},
+		createPerson: {
+			method: 'POST'
+		}
+	})
+}]);
+
+app.factory('HentPerson', ['$resource', function($resource) {
+	return $resource(CONFIG.API_BASE_URL + ":id", {}, {
+		hentPerson: {
+			method: 'GET'
+		}
+	})
+}]);
+
+
+app.factory('Kjenner', ['$resource', function($resource) {
+	return $resource(CONFIG.API_BASE_URL + ":id/kjenner/:kjenner_id", {}, {
+		addKjenner: {
+			method: 'PUT'
+		}
+	})
+}]);
+
+app.factory('HarSett', ['$resource', function($resource) {
+	return $resource(CONFIG.API_BASE_URL + ":id/harSett", {}, {
+		harSett: {
+			method: 'PUT'
+		}
+	})
+}]);
+
+
 })(angular.module('app'));
