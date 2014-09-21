70ecfc5c48b90a7db89f75d93006b58b06ce1104
mapster <mapster@ndt-lan.no>
2014-09-21 15:45:47 +0200

    la til enda mer access control ting

--- service/src/main/java/com/computas/sem/uib/provider/PersonProvider.java
+++ service/src/main/java/com/computas/sem/uib/provider/PersonProvider.java
@@ -67,6 +67,7 @@ public class PersonProvider {
 				.header("Access-Control-Allow-Origin", "*")
 				.header("Access-Control-Allow-Methods",	"POST, GET, PUT, UPDATE, OPTIONS")
 				.header("Access-Control-Allow-Headers",	"Content-Type, Accept, X-Requested-With, cx_auth, cx_secret")
+				.header("Access-Control-Expose-Headers", "cx_auth, cx_secret")
 				.build();
 	}
 	
