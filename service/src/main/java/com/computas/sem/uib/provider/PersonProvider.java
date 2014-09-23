002c9f5368019d5a17d9be1d89cf786b1d9267a3
mapster <mapster@ndt-lan.no>
2014-09-23 08:31:46 +0200

    fjernet et options endepunkt

--- service/src/main/java/com/computas/sem/uib/provider/PersonProvider.java
+++ service/src/main/java/com/computas/sem/uib/provider/PersonProvider.java
@@ -60,18 +60,18 @@ public class PersonProvider {
 				.build();
 	}
 	
-	@OPTIONS
-	@PermitAll
-	@javax.annotation.security.RolesAllowed("")
-	@Path("{path:.*}")
-	public Response optionsAll(@PathParam("path") String path) {
-		return Response
-				.ok()
-				.header("Access-Control-Allow-Origin", "*")
-				.header("Access-Control-Allow-Methods",	"POST, GET, PUT, UPDATE, OPTIONS")
-				.header("Access-Control-Allow-Headers",	"Content-Type, Accept, X-Requested-With, cx_auth")
-				.build();
-	}
+//	@OPTIONS
+//	@PermitAll
+//	@javax.annotation.security.RolesAllowed("")
+//	@Path("{path:.*}")
+//	public Response optionsAll(@PathParam("path") String path) {
+//		return Response
+//				.ok()
+//				.header("Access-Control-Allow-Origin", "*")
+//				.header("Access-Control-Allow-Methods",	"POST, GET, PUT, UPDATE, OPTIONS")
+//				.header("Access-Control-Allow-Headers",	"Content-Type, Accept, X-Requested-With, cx_auth")
+//				.build();
+//	}
 	
 	@GET
 	@Produces(MEDIA_TYPE)
